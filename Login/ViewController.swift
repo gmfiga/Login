a8e99ec02fc627654f5ed134d4473016ec11174e
Administrator <administrator@gbtc408013dt.montgomerycollege.edu>
2018-06-27 15:47:39 -0400

    Works

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btnForgotPassword: UIButton!
+    @IBOutlet weak var btnForgotUsername: UIButton!
+    @IBOutlet weak var txtUsername: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    @IBAction func actionForgotUsername(_ sender: Any) {
+        performSegue(withIdentifier: "Login", sender: btnForgotUsername)
+    }
+    
+    @IBAction func actionForgotPassword(_ sender: Any) {
+        performSegue(withIdentifier: "Login", sender: btnForgotPassword)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let sender = sender as? UIButton else {return}
+        
+        if sender == btnForgotPassword {
+            segue.destination.navigationItem.title = "Forgot Password"
+        }else if sender == btnForgotUsername{
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = txtUsername.text
+        }
     }
 
     override func didReceiveMemoryWarning() {
